246019dbf9eedabe3ed53d414a50ca90b3fd0226
Murali Krishna <vmurali1444@gmail.com>
2024-05-28 22:53:38 +0530

    feat: add basic htmx working

--- index.tsx
+++ index.tsx
@@ -1,20 +1,26 @@
 import Elysia from "elysia";
 import { html } from "@elysiajs/html";
-import * as elements from "typed-html";
+import type { Children } from "typed-html";
 
-const BaseHtml = ({ children }: elements.Children) =>
+const app = new Elysia()
+    .use(html())
+    .get("/", ({ html }) => html(
+        <BaseHtml>
+            <button hx-post="/clicked" hx-swap="outerHTML">get server response</button>
+        </BaseHtml>
+    ))
+    .post("/clicked", <p>Response from server</p>)
+    .listen(3000);
+
+console.log(`Server started on port: http://localhost:${app.server?.port}`)
+
+const BaseHtml = ({ children }: Children) =>
     <html lang="en">
         <head>
             <title>📜 Todo app</title>
+            <script src="https://unpkg.com/htmx.org@1.9.12"></script>
         </head>
         <body>
             {children}
         </body>
     </html>
-
-const app = new Elysia()
-    .use(html())
-    .get("/", ({ html }) => html(<BaseHtml>Hello World</BaseHtml>))
-    .listen(3000);
-
-console.log(`Server started on port: http://localhost:${app.server?.port}`)
