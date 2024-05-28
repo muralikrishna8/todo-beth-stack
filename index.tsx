f64be197578800d4f20623099735e1779380567d
Murali Krishna <vmurali1444@gmail.com>
2024-05-28 23:03:45 +0530

    feat: add tailwind css

--- index.tsx
+++ index.tsx
@@ -9,7 +9,7 @@ const app = new Elysia()
             <button hx-post="/clicked" hx-swap="outerHTML">get server response</button>
         </BaseHtml>
     ))
-    .post("/clicked", <p>Response from server</p>)
+    .post("/clicked", <p class="text-blue-600">Response from server</p>)
     .listen(3000);
 
 console.log(`Server started on port: http://localhost:${app.server?.port}`)
@@ -19,8 +19,9 @@ const BaseHtml = ({ children }: Children) =>
         <head>
             <title>📜 Todo app</title>
             <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+            <script src="https://cdn.tailwindcss.com"></script>
         </head>
-        <body>
+        <body class="flex w-full h-screen justify-center items-center">
             {children}
         </body>
     </html>
