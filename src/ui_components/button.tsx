74c1017b2ecb0f4caba563aaf10cd7b1395024b5
Murali Krishna <vmurali1444@gmail.com>
2024-05-31 01:24:01 +0530

    style: add background box for todos and fix the button color

--- src/ui_components/button.tsx
+++ src/ui_components/button.tsx
@@ -2,7 +2,7 @@ import type { Children } from "typed-html";
 
 export const Button = ({ type, children }: ({ type: string, children: Children })) =>
     <button
-        class="bg-indigo-500 px-5 py-1 rounded"
+        class="bg-[#719191] h-10 px-6 font-semibold rounded-md"
         type={type}>
         {children}
     </button>
--- src/view/todos.tsx
+++ src/view/todos.tsx
@@ -2,7 +2,7 @@ import type { Todo } from "../db/schema";
 import { TodoForm } from "./todoForm";
 import { TodoItem } from "./todoItem";
 
-export const TodoList = ({ todos }: { todos: Todo[] }) => <ul>
+export const TodoList = ({ todos }: { todos: Todo[] }) => <ul class="p-5 bg-white rounded-lg shadow-sm">
     {todos.map(todo => <li><TodoItem {...todo}/></li>)}
     <TodoForm />
 </ul>
