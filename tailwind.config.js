e7dbd6871a2ac033a9d7e134bda5ce580ccaca68
Murali Krishna <vmurali1444@gmail.com>
2024-05-31 01:25:21 +0530

    chore: update tailwind config with the theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        'greenish': {
+          primary: '#719191',
+          DEFAULT: '#719191',
+          secondary: '#eaf1f1',
+          tertiary: '#fafafa',
+          accent: '#eb9a78'
+        },
+      },
+    },
+  }
