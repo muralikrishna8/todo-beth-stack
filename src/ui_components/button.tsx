1e15296e368c1d07d6be36d59d05d13a0276a8f2
Murali Krishna <vmurali1444@gmail.com>
2024-05-31 17:42:37 +0530

    fix: type for the children in Button component

--- src/ui_components/button.tsx
+++ src/ui_components/button.tsx
@@ -1,6 +1,4 @@
-import type { Children } from "typed-html";
-
-export const Button = ({ type, children }: ({ type: string, children: Children })) =>
+export const Button = ({ type, children }: ({ type: string, children: HTMLElement | string })) =>
     <button
         class="bg-greenish-primary h-10 px-6 font-semibold rounded-full"
         type={type}>
