fb882a4abc974402b4b0cdcca2c5f16ce3ffd5b6
Murali Krishna <vmurali1444@gmail.com>
2024-06-03 22:36:42 +0530

    fix: #2 Dockerfile - fix generating css

---
+++ src/ui_components/checkbox.tsx
@@ -0,0 +1,21 @@
+const Checkbox = (params: { checked: boolean, id: string }) =>
+    <label
+        class="relative flex items-center rounded-full cursor-pointer"
+        for={`${params.id}`}>
+        <input
+            type="checkbox"
+            {...params}
+            class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-greenish-accent checked:bg-greenish-accent"
+        />
+        <span
+            class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                stroke="currentColor" stroke-width="1">
+                <path fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"></path>
+            </svg>
+        </span>
+    </label>;
+
+export default Checkbox;
--- src/view/todoItem.tsx
+++ src/view/todoItem.tsx
@@ -1,20 +1,27 @@
 import type { Todo } from "../db/schema";
+import Checkbox from "../ui_components/checkbox";
 
-export const TodoItem = ({ content, completed, id }: Todo) =>
-    <div class="flex flex-row space-x-3 m-3">
-        <input
-            type="checkbox"
-            checked={completed}
-            hx-post={`/todos/toggle/${id}`}
-            hx-target="closest div"
-            hx-swap="outerHTML"
-            class="accent-[#eb9a78] w-4"
-        />
-        <p>{content}</p>
+export const TodoItem = ({ content, completed, id }: Todo) => {
+    const todoId = `todo_${id}`;
+
+    return <div id={todoId} class="flex flex-row space-x-3 m-3">
+        <div class="inline-flex items-center">
+            <Checkbox
+                checked={completed}
+                id={`${id}`}
+                hx-post={`/todos/toggle/${id}`}
+                hx-swap="outerHTML"
+                hx-target={`#${todoId}`}
+            ></Checkbox>
+        </div>
+        <p
+            id={id}
+            class={completed ? "text-greenish-primary line-through decoration-1 opacity-50" : ""}>{content}</p>
         <button
             hx-delete={`/todos/${id}`}
             hx-target="closest div"
             hx-swap="outerHTML"
             title="delete todo"
         >🗑️</button>
-    </div>
+    </div>;
+}
